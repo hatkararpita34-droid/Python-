9e8e746db4008287edc29aff7360ad6773242818
hatkararpita34-droid <hatkararpita34@gmail.com>
2026-03-21 19:41:58 +0530

    Create python_datatypes.py

---
+++ python_datatypes.py
@@ -0,0 +1,43 @@
+# Integer
+a = 12
+print(a)
+print(type(a))
+
+# Float
+marks = 81.25
+print(marks)
+print(type(marks))
+
+# String
+name = "Arpita"
+print(name)
+print(type(name))
+
+# Boolean
+b = False
+print(b)
+print(type(b))
+
+# List
+x = [11, 22, 33, 44, 55]
+print(x)
+print(type(x))
+
+# Tuple
+numbers = (7, 14, 21, 28)
+print(numbers)
+print(type(numbers))
+
+# Dictionary
+xyz = {
+"Name": "Arpita",
+"Age": 19,
+"Course": "Civil Engineering"
+}
+print(xyz)
+print(type(xyz))
+
+# Set
+colors = {"Black", "White", "Grey"}
+print(colors)
+print(type(colors))
